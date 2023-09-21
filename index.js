1ae5c7824db25c36e58d99ff2714d3b5dc66d672
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2023-09-21 14:28:04 -0500

    init commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const app = express();
+
+require('dotenv').config();
+
+const PORT = 8080 || process.env.PORT;
+
+const people = [
+    { name: 'John' },
+    { name: 'Doe' },
+    { name: 'Smith' },
+    { name: 'Chris' },
+    { name: 'Mark' },
+    { name: 'Aydin' },
+];
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.get('/people', (req, res) => {
+    res.json(people);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
